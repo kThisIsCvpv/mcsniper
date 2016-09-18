c9d43db7706f423a5502534efc57425174f03ace
BuildTools <admin@kthisiscvpv.com>
2016-09-17 23:58:24 -0400

    Full Beta Version R1

---
+++ src/main/java/co/mcsniper/mcsniper/sniper/util/WorldTime.java
@@ -0,0 +1,74 @@
+package co.mcsniper.mcsniper.sniper.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class WorldTime {
+
+	private String[] timeDelay = new String[20];
+	private Thread timedCalculator;
+
+	public WorldTime() {
+		calculateDelay(true);
+
+		timedCalculator = new Thread(new Runnable() {
+			public void run() {
+				while (true)
+					calculateDelay(false);
+			}
+		});
+
+		timedCalculator.start();
+	}
+
+	public long getDelay() {
+		long currentDelay = 0;
+		for (int i = 0; i < timeDelay.length; i++)
+			if (timeDelay[i] != null)
+				currentDelay += Long.parseLong(timeDelay[i]);
+			else
+				return currentDelay / (i + 1);
+		return currentDelay / timeDelay.length;
+	}
+
+	public void calculateDelay(boolean debug) {
+		for (int i = 0; i < timeDelay.length; i++) {
+			boolean failed = true;
+			while (failed) {
+				try {
+					if (!debug)
+						Thread.sleep(2500L);
+
+					URL url = new URL("https://www.kthisiscvpv.com/time.php");
+
+					URLConnection connection = url.openConnection();
+					long requestTime = System.currentTimeMillis();
+
+					BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					String parsed = new String();
+
+					String reader;
+					while ((reader = br.readLine()) != null)
+						parsed += reader;
+
+					String readLine = new String(parsed.trim().replaceAll("\\.", "") + "0000").substring(0, 13);
+					long serverTime = Long.parseLong(readLine) - 30 + ((System.currentTimeMillis() - requestTime) / 2L);
+					timeDelay[i] = Long.toString(serverTime - System.currentTimeMillis());
+
+					br.close();
+					failed = false;
+				} catch (Exception ignorred) { }
+			}
+		}
+	}
+
+	public void kill() {
+		timedCalculator.interrupt();
+	}
+
+	public long currentTimeMillis() {
+		return System.currentTimeMillis() + this.getDelay();
+	}
+}
