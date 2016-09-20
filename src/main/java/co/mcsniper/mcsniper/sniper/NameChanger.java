a4914c2cc004197b15a8cf6c0b41a93788cc855e
BuildTools <admin@kthisiscvpv.com>
2016-09-19 22:38:53 -0400

    WebClient Testing R3

--- src/main/java/co/mcsniper/mcsniper/sniper/NameChanger.java
+++ src/main/java/co/mcsniper/mcsniper/sniper/NameChanger.java
@@ -1,11 +1,17 @@
 package co.mcsniper.mcsniper.sniper;
 
 import java.net.Proxy;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.lyphiard.simplerequest.RequestType;
-import com.lyphiard.simplerequest.SimpleHttpRequest;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.util.Cookie;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 
 public class NameChanger implements Runnable {
 
@@ -35,7 +41,7 @@ public class NameChanger implements Runnable {
         this.password = password;
         this.log = log;
 
-        String rawSession = this.session;
+        String rawSession = new String(this.session);
         if (rawSession.startsWith("\"") && rawSession.endsWith("\"") && rawSession.length() > 2) {
             rawSession = rawSession.substring(1, rawSession.length() - 1);
         }
@@ -62,53 +68,72 @@ public class NameChanger implements Runnable {
         }
     }
 
-    public void run() {
-//        WebClient client = null;
-//        
-        try {
-//            WebRequest request = new WebRequest(new URL(this.url), HttpMethod.POST);
-//            
-//            request.setAdditionalHeader("Accept", "*/*");
-//            request.setAdditionalHeader("Accept-Encoding", "gzip, deflate, br");
-//            request.setAdditionalHeader("Accept-Language", "en-US,en;q=0.8");
-//            request.setAdditionalHeader("Connection", "keep-alive");
-//            request.setAdditionalHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-//            request.setAdditionalHeader("DNT", "1");
-//            request.setAdditionalHeader("Host", "account.mojang.com");
-//            request.setAdditionalHeader("Origin", "https://account.mojang.com");
-//            request.setAdditionalHeader("Referer", this.url);
-//            request.setAdditionalHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
-//            request.setAdditionalHeader("X-Requested-With", "XMLHttpRequest");
-//            
-//            request.setRequestParameters(new ArrayList<NameValuePair>());
-//            request.getRequestParameters().add(new NameValuePair("authenticityToken", this.authToken));
-//            request.getRequestParameters().add(new NameValuePair("newName", this.name));
-//            request.getRequestParameters().add(new NameValuePair("password", this.password));
+//    public void run() {        
+//        try {               
+//            String response = new SimpleHttpRequest(this.url)
+//                    .setProxy(this.proxy)
+//                    .addField("authenticityToken", this.authToken)
+//                    .addField("newName", this.name)
+//                    .addField("password", this.password)
+//                    .setType(RequestType.POST)
+//                    .setTimeout(30000)
+//                    .addHeader("Accept-Language", "en-US,en;q=0.8")
+//                    .setCookie("PLAY_SESSION", this.session)
+//                    .execute()
+//                    .getResponse();
 //
-//            String fullAddress = this.proxy.toString().split("/")[1];
-//            String proxyAddress = fullAddress.split(":")[0];
-//            int proxyPort = Integer.parseInt(fullAddress.split(":")[1]);
-//            
-//            client = new WebClient(BrowserVersion.CHROME, proxyAddress, proxyPort);
-//            client.getOptions().setJavaScriptEnabled(false);
-//            client.getOptions().setCssEnabled(false);
-//            client.getOptions().setThrowExceptionOnFailingStatusCode(false);
-//            client.getOptions().setThrowExceptionOnScriptError(false);
-//            client.getOptions().setTimeout(30000);
-//            
-//            String response = client.getPage(request).getWebResponse().getContentAsString();
-                        
-            String response = new SimpleHttpRequest(this.url)
-                    .setProxy(this.proxy)
-                    .addField("authenticityToken", this.authToken)
-                    .addField("newName", this.name)
-                    .addField("password", this.password)
-                    .setType(RequestType.POST)
-                    .setTimeout(30000)
-                    .addHeader("Accept-Language", "en-US,en;q=0.8")
-                    .setCookie("PLAY_SESSION", this.session)
-                    .execute()
-                    .getResponse();
+//            long endTime = this.main.getHandler().getWorldTime().currentTimeMillis();
+//
+//            this.log[this.server][this.instance][0] = response;
+//            this.log[this.server][this.instance][1] = (endTime - this.main.getDate()) + "";
+//
+//            if (response.contains("Name changed")) {
+//                this.main.setSuccessful();
+//            }
+//        } catch (Exception ex) {
+//            this.log[this.server][this.instance][0] = ex.getClass().getSimpleName() + (ex.getMessage() != null ? ": " + ex.getMessage() : "");
+//
+//            long endTime = this.main.getHandler().getWorldTime().currentTimeMillis();
+//            this.log[this.server][this.instance][1] = (endTime - this.main.getDate()) + "";
+//        }
+//    }
+    
+    public void run() {
+        WebClient client = null;
+        
+        try {
+            WebRequest request = new WebRequest(new URL(this.url), HttpMethod.POST);
+            
+            request.setAdditionalHeader("Accept", "*/*");
+            request.setAdditionalHeader("Accept-Encoding", "gzip, deflate, br");
+            request.setAdditionalHeader("Accept-Language", "en-US,en;q=0.8");
+            request.setAdditionalHeader("Connection", "keep-alive");
+            request.setAdditionalHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            request.setAdditionalHeader("DNT", "1");
+            request.setAdditionalHeader("Host", "account.mojang.com");
+            request.setAdditionalHeader("Origin", "https://account.mojang.com");
+            request.setAdditionalHeader("Referer", this.url);
+            request.setAdditionalHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
+            request.setAdditionalHeader("X-Requested-With", "XMLHttpRequest");
+            
+            request.setRequestParameters(new ArrayList<NameValuePair>());
+            request.getRequestParameters().add(new NameValuePair("authenticityToken", this.authToken));
+            request.getRequestParameters().add(new NameValuePair("newName", this.name));
+            request.getRequestParameters().add(new NameValuePair("password", this.password));
+
+            String fullAddress = this.proxy.toString().split("/")[1];
+            String proxyAddress = fullAddress.split(":")[0];
+            int proxyPort = Integer.parseInt(fullAddress.split(":")[1]);
+            
+            client = new WebClient(BrowserVersion.CHROME, proxyAddress, proxyPort);
+            client.getOptions().setJavaScriptEnabled(false);
+            client.getOptions().setCssEnabled(false);
+            client.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            client.getOptions().setThrowExceptionOnScriptError(false);
+            client.getOptions().setTimeout(30000);
+            client.getCookieManager().addCookie(new Cookie("account.mojang.com", "PLAY_SESSION", this.session));
+            
+            String response = client.getPage(request).getWebResponse().getContentAsString();
 
             long endTime = this.main.getHandler().getWorldTime().currentTimeMillis();
 
@@ -123,14 +148,14 @@ public class NameChanger implements Runnable {
 
             long endTime = this.main.getHandler().getWorldTime().currentTimeMillis();
             this.log[this.server][this.instance][1] = (endTime - this.main.getDate()) + "";
-//        } finally {
-//            if(client != null) {
-//                try {
-//                    client.close();
-//                } catch (Exception ex) {
-//                    
-//                }
-//            }
+        } finally {
+            if(client != null) {
+                try {
+                    client.close();
+                } catch (Exception ex) {
+                    
+                }
+            }
         }
     }
 
