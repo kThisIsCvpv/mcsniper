70f1ecd35b9af92016081250ff2bde349445f8f4
Charles Xu <admin@Kthisiscvpv.com>
2016-12-20 12:30:46 -0500

    Auto Proxies Update

--- src/main/java/co/mcsniper/Main.java
+++ src/main/java/co/mcsniper/Main.java
@@ -11,5 +11,4 @@ public class Main {
             ex.printStackTrace();
         }
     }
-
 }
--- src/main/java/co/mcsniper/mcsniper/sniper/proxy/ProxyHandler.java
+++ src/main/java/co/mcsniper/mcsniper/sniper/proxy/ProxyHandler.java
@@ -10,61 +10,115 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+
 import co.mcsniper.mcsniper.MCSniper;
 
 public class ProxyHandler {
 
-    private List<Proxy> availableProxies = new ArrayList<>();
-    private int currentProxy = 0;
+	private List<Proxy> availableProxies = new ArrayList<>();
+	private int currentProxy = 0;
 
-    public ProxyHandler(MCSniper sniper) throws IOException {
+	public ProxyHandler(MCSniper sniper) throws IOException {
+		try {
+			Connection connection = sniper.getMySQL().createConnection();
 
-        try {
-            Connection connection = sniper.getMySQL().createConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM proxy_list WHERE node = ?;");
+			preparedStatement.setString(1, sniper.getServerName());
 
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM proxies WHERE node = ? AND enabled = 1");
-            preparedStatement.setString(1, sniper.getServerName());
-            preparedStatement.setFetchSize(100);
-            ResultSet resultSet = preparedStatement.executeQuery();
+			ResultSet resultSet = preparedStatement.executeQuery();
+			String url = null;
 
-            while (resultSet.next()) {
-                this.availableProxies.add(new Proxy(Proxy.Type.valueOf(resultSet.getString("type")), new InetSocketAddress(resultSet.getString("ip"), resultSet.getInt("port"))));
-            }
+			while (resultSet.next()) {
+				url = resultSet.getString("url");
+			}
 
-            resultSet.close();
-            preparedStatement.close();
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			resultSet.close();
+			preparedStatement.close();
+			connection.close();
 
-    public List<Proxy> getAllProxies() {
-        return this.availableProxies;
-    }
+			if (url != null) {
+				this.availableProxies = listProxies(url);
+				System.out.println("Successfully loaded " + this.availableProxies + " different entries!");
+			} else {
+				throw new Exception("Unable to find proxy list for server!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    public List<Proxy> getProxies(int amount) {
-        List<Proxy> returnList = new ArrayList<>();
-        for (int i = 0; i < amount; i++) {
-            returnList.add(getNextProxy());
-        }
+		// try {
+		// Connection connection = sniper.getMySQL().createConnection();
+		//
+		// PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM proxies WHERE node = ? AND enabled = 1");
+		// preparedStatement.setString(1, sniper.getServerName());
+		// preparedStatement.setFetchSize(100);
+		// ResultSet resultSet = preparedStatement.executeQuery();
+		//
+		// while (resultSet.next()) {
+		// this.availableProxies.add(new Proxy(Proxy.Type.valueOf(resultSet.getString("type")), new InetSocketAddress(resultSet.getString("ip"), resultSet.getInt("port"))));
+		// }
+		//
+		// resultSet.close();
+		// preparedStatement.close();
+		// connection.close();
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+	}
 
-        return returnList;
-    }
+	private List<Proxy> listProxies(String url) {
+		WebClient client = new WebClient();
+		List<Proxy> proxies = new ArrayList<Proxy>();
 
-    private Proxy getNextProxy() {
-        Proxy returnProxy = this.availableProxies.get(this.currentProxy);
+		try {
+			Page page = client.getPage(url);
+			String[] dataList = page.getWebResponse().getContentAsString().split("\n");
 
-        this.currentProxy++;
-        if (this.currentProxy >= this.availableProxies.size()) {
-            this.currentProxy = 0;
-        }
+			for (String data : dataList) {
+				String parsed = data.trim();
+				if (parsed.contains(":")) {
+					String ip = parsed.split(":")[0];
+					int port = Integer.parseInt(parsed.split(":")[1]);
+					proxies.add(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port)));
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			client.close();
+		}
 
-        return returnProxy;
-    }
+		return proxies;
+	}
 
-    public void shuffle() {
-        Collections.shuffle(this.availableProxies);
-    }
+	public List<Proxy> getAllProxies() {
+		return this.availableProxies;
+	}
+
+	public List<Proxy> getProxies(int amount) {
+		List<Proxy> returnList = new ArrayList<>();
+		for (int i = 0; i < amount; i++) {
+			returnList.add(getNextProxy());
+		}
+
+		return returnList;
+	}
+
+	private Proxy getNextProxy() {
+		Proxy returnProxy = this.availableProxies.get(this.currentProxy);
+
+		this.currentProxy++;
+		if (this.currentProxy >= this.availableProxies.size()) {
+			this.currentProxy = 0;
+		}
+
+		return returnProxy;
+	}
+
+	public void shuffle() {
+		Collections.shuffle(this.availableProxies);
+	}
 
 }
