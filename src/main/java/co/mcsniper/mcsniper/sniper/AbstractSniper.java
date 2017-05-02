e672963ff2368327e8c52d36634e72b2656b64d8
James <admin@lyph.io>
2017-05-02 13:28:27 -0400

    Decrease K_OFFSET to 0.45 from 0.60

--- src/main/java/co/mcsniper/mcsniper/sniper/AbstractSniper.java
+++ src/main/java/co/mcsniper/mcsniper/sniper/AbstractSniper.java
@@ -13,7 +13,7 @@ public abstract class AbstractSniper implements Runnable {
     /**
      * The offset for each sequential request
      */
-    private static double K_OFFSET = 0.6;
+    private static double K_OFFSET = 0.45;
 
     private String name;
     private int snipeId;
